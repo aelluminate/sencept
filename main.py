0044276bb43a9d36f38505b5249b892ffd6d85a3
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-27 03:55:35 +0800

    feat: add month parameter as an option

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from datetime import datetime
 
 if __name__ == "__main__":
     # Change this number to generate rows
-    num_rows_to_generate = 723772
+    num_rows_to_generate = 523523
+    month = 2
+    year = 2024
 
     # Generate synthetic data
-    synthetic_df = generate_synthetic_data(num_rows_to_generate, year=2024)
+    synthetic_df = generate_synthetic_data(num_rows_to_generate, month=month, year=year)
 
     # Create a filename with the current datetime
     current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
     output_dir = "data"
-    output_path = (
-        f"{output_dir}/synthetic_data_{current_datetime}_{num_rows_to_generate}.csv"
-    )
+    output_path = f"{output_dir}/synthetic_data_{current_datetime}_m{month}y{year}_{num_rows_to_generate}.csv"
 
     # Create the directory if it doesn't exist
     if not os.path.exists(output_dir):
