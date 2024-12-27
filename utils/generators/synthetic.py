c4d383bc57d54181718a362c44474662354a6181
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-28 07:25:14 +0800

    fix: remove unused parameters

--- utils/generators/synthetic.py
+++ utils/generators/synthetic.py
@@ -2,9 +2,7 @@ from utils.load_schema import load_schema
 from utils.generators.schema import generate_data_from_schema
 
 
-def generate_synthetic_data(
-    num_rows, month=None, year=None, schema_file="generate.json"
-):
+def generate_synthetic_data(num_rows, schema_file="generate.json"):
     schema = load_schema(schema_file)
     if schema is None:
         raise ValueError(f"Failed to load schema file {schema_file}")
