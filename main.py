3fd31474abfda3870acf0e01e5504bf0fd2a97c6
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-28 07:24:50 +0800

    fix: remove month and year parameter

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from datetime import datetime
 if __name__ == "__main__":
     # Change this number to generate rows
     num_rows_to_generate = 423221
-    month = 2
-    year = 2024
 
     # Generate synthetic data
-    synthetic_df = generate_synthetic_data(num_rows_to_generate, month=month, year=year)
+    synthetic_df = generate_synthetic_data(num_rows_to_generate)
 
     # Create a filename with the current datetime
     current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
     output_dir = "data/generated/"
-    output_path = f"{output_dir}/synthetic_data_{current_datetime}_m{month}y{year}_{num_rows_to_generate}.csv"
+    output_path = (
+        f"{output_dir}/synthetic_data_{current_datetime}_{num_rows_to_generate}.csv"
+    )
 
     # Create the directory if it doesn't exist
     if not os.path.exists(output_dir):
