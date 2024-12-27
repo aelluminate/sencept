d799cd65a2c2108cdd8e624c6febdb64a8c626d3
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-27 09:58:59 +0800

    pref: optimize data generation logic

--- lib/data_generator.py
+++ lib/data_generator.py
@@ -6,6 +6,12 @@ from tqdm import tqdm
 from datetime import timedelta, datetime
 from constants.categories import product_categories_list, product_categories_weights
 from constants.coupons import special_events
+import logging
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 fake = Faker("en_US")
 total_weight = sum(product_categories_weights)
@@ -13,36 +19,106 @@ product_categories_weights = [w / total_weight for w in product_categories_weigh
 
 
 def is_special_event(date):
-    for event_date in special_events.values():
-        if date == event_date.date():
-            return True
-    return False
+    return date in (event_date.date() for event_date in special_events.values())
 
 
 def generate_synthetic_data(num_rows=1000, month=None, year=None):
     data = []
     existing_users = {}
 
-    for _ in tqdm(range(num_rows), desc="Generating data", unit="record"):
-
-        if np.random.random() < 0.1 and existing_users:
-            user_id, phone_number = random.choice(list(existing_users.items()))
-        else:
-            user_id = f"U{random.randint(1000000, 9999999)}"
-            phone_number = f"+63{random.randint(1000000, 9999999)}"
-            existing_users[user_id] = phone_number
-
+    logging.info("Generating User Profiles")
+    for _ in range(num_rows):
+        user_id = f"****{random.randint(1000000, 9999999)}"
+        phone_number = f"+63{random.randint(1000000, 9999999)}"
         age = np.random.randint(18, 65)
         sex = np.random.choice(["M", "F", "O"])
         join_date = fake.date_between(start_date="-5y", end_date="today")
+        payment_method = np.random.choice(
+            [
+                "GCash",
+                "Maya",
+                "GrabPay",
+                "ShopeePay",
+                "Coins.ph",
+                "Visa (Credit)",
+                "Mastercard (Credit)",
+                "American Express",
+                "Visa (Debit)",
+                "Mastercard (Debit)",
+                "Cash",
+                "Bank Transfer",
+                "Over-the-counter (OTC)",
+            ]
+        )
+        loyalty_program_member = np.random.choice([0, 1])
+        loyalty_tier = (
+            np.random.choice([0, 1, 2, 3, 4]) if loyalty_program_member else None
+        )
 
+        existing_users[user_id] = {
+            "phone_number": phone_number,
+            "age": age,
+            "sex": sex,
+            "join_date": join_date,
+            "country": "Philippines",
+            "payment_method": payment_method,
+            "loyalty_program_member": loyalty_program_member,
+            "loyalty_tier": loyalty_tier,
+        }
+
+    logging.info("Starting data generation loop")
+    for _ in tqdm(range(num_rows), desc="Generating data", unit="record"):
+        if np.random.random() < 0.1 and existing_users:
+            user_id = random.choice(list(existing_users.keys()))
+        else:
+            user_id = f"****{random.randint(1000000, 9999999)}"
+            phone_number = f"+63{random.randint(1000000, 9999999)}"
+            age = np.random.randint(18, 65)
+            sex = np.random.choice(["M", "F", "O"])
+            join_date = fake.date_between(start_date="-5y", end_date="today")
+            payment_method = np.random.choice(["credit", "debit", "cash"])
+            loyalty_program_member = np.random.choice([0, 1])
+            loyalty_tier = (
+                np.random.choice([0, 1, 2, 3, 4]) if loyalty_program_member else None
+            )
+
+            existing_users[user_id] = {
+                "phone_number": phone_number,
+                "age": age,
+                "sex": sex,
+                "join_date": join_date,
+                "country": "Philippines",
+                "payment_method": payment_method,
+                "loyalty_program_member": loyalty_program_member,
+                "loyalty_tier": loyalty_tier,
+            }
+
+        user_profile = existing_users[user_id]
         order_id = f"******{np.random.randint(1000, 9999)}"
         total_purchase = round(np.random.uniform(100, 10000), 2)
+        tier_discount_percentage = {0: 0, 1: 3, 2: 5, 3: 7, 4: 10}.get(
+            user_profile["loyalty_tier"], 0
+        )
+        coupon_discount_percentage = (
+            np.random.randint(2, 5)
+            if is_special_event(fake.date_between(start_date="-5y", end_date="today"))
+            else 0
+        )
+        total_discount_percentage = (
+            tier_discount_percentage + coupon_discount_percentage
+        )
+        total_discount = round(total_purchase * (total_discount_percentage / 100), 2)
+        total_purchase_after_discount = round(total_purchase - total_discount, 2)
+        product_category = np.random.choice(
+            product_categories_list, p=product_categories_weights
+        )
+        purchase_medium = np.random.choice(["online", "in-store"])
+        customer_exp_rating = np.random.randint(1, 5)
 
         if year and month:
             start_date = datetime(year, month, 1)
             end_date = (
-                datetime(year, month + 1, 1) - timedelta(days=1)
+                (datetime(year, month + 1, 1) - timedelta(days=1))
                 if month < 12
                 else datetime(year, 12, 31)
             )
@@ -52,35 +128,12 @@ def generate_synthetic_data(num_rows=1000, month=None, year=None):
             end_date = datetime(year, 12, 31)
             purchase_date = fake.date_between(start_date=start_date, end_date=end_date)
         else:
-            purchase_date = fake.date_between(start_date=join_date, end_date="today")
+            purchase_date = fake.date_between(
+                start_date=user_profile["join_date"], end_date="today"
+            )
 
         purchase_time = fake.time()
         purchased_at = f"{purchase_date} {purchase_time}"
-        payment_method = np.random.choice(["credit", "debit", "cash"])
-
-        loyalty_program_member = np.random.choice([0, 1])
-        loyalty_tier = (
-            np.random.choice([1, 2, 3, 4]) if loyalty_program_member else None
-        )
-        tier_discount_percentage = {1: 3, 2: 5, 3: 7, 4: 10}.get(loyalty_tier, 0)
-
-        coupon_discount_percentage = 0
-        if is_special_event(purchase_date):
-            coupon_discount_percentage = np.random.randint(2, 5)
-
-        total_discount_percentage = (
-            tier_discount_percentage + coupon_discount_percentage
-        )
-        total_discount = round(total_purchase * (total_discount_percentage / 100), 2)
-        total_purchase_after_discount = round(total_purchase - total_discount, 2)
-
-        product_category = np.random.choice(
-            product_categories_list,
-            replace=False,
-            p=product_categories_weights,
-        )
-        purchase_medium = np.random.choice(["online", "in-store"])
-        customer_exp_rating = np.random.randint(1, 5)
 
         release_date = fake.date_between(
             start_date=purchase_date, end_date=purchase_date + timedelta(days=3)
@@ -96,14 +149,14 @@ def generate_synthetic_data(num_rows=1000, month=None, year=None):
         data.append(
             {
                 "user_id": user_id,
-                "age": age,
-                "sex": sex,
-                "phone_number": phone_number,
-                "join_date": join_date,
-                "country": "Philippines",
-                "payment_method": payment_method,
-                "loyalty_program_member": loyalty_program_member,
-                "loyalty_tier": loyalty_tier,
+                "age": user_profile["age"],
+                "sex": user_profile["sex"],
+                "phone_number": user_profile["phone_number"],
+                "join_date": user_profile["join_date"],
+                "country": user_profile["country"],
+                "payment_method": user_profile["payment_method"],
+                "loyalty_program_member": user_profile["loyalty_program_member"],
+                "loyalty_tier": user_profile["loyalty_tier"],
                 "tier_discount_percentage": tier_discount_percentage,
                 "coupon_discount_percentage": coupon_discount_percentage,
                 "total_discount_percentage": total_discount_percentage,
@@ -121,4 +174,5 @@ def generate_synthetic_data(num_rows=1000, month=None, year=None):
             }
         )
 
+    logging.info("Data generation complete")
     return pd.DataFrame(data)
