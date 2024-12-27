33dc22da639924f846e7eca362dd9564621c5b6c
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-27 18:06:08 +0800

    refactor: update import paths

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from lib.data_generator import generate_synthetic_data
-from lib.file_operations import save_to_csv
+from utils.data_generator import generate_synthetic_data
+from utils.file_operations import save_to_csv
 from datetime import datetime
 
 if __name__ == "__main__":
     # Change this number to generate rows
-    num_rows_to_generate = 523523
+    num_rows_to_generate = 294252
     month = 2
     year = 2024
 
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     # Create a filename with the current datetime
     current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_dir = "data"
+    output_dir = "data/generated/"
     output_path = f"{output_dir}/synthetic_data_{current_datetime}_m{month}y{year}_{num_rows_to_generate}.csv"
 
     # Create the directory if it doesn't exist
