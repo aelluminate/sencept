7d477640765750acfe06d53d2ad92790adf02619
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-28 11:41:36 +0800

    feat: apply CLI version

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import os
-from utils.generators.synthetic import generate_synthetic_data
-from utils.file_operations import save_to_csv
+import argparse
 from datetime import datetime
+from utils.synthetic import generate_synthetic_data
+from utils.file_operations import save_to_csv, save_to_json, save_to_excel
 
-if __name__ == "__main__":
-    # Change this number to generate rows
-    num_rows_to_generate = 423221
 
-    # Generate synthetic data
-    synthetic_df = generate_synthetic_data(num_rows_to_generate)
-
-    # Create a filename with the current datetime
-    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_dir = "data/generated/"
-    output_path = (
-        f"{output_dir}synthetic_data_{current_datetime}_{num_rows_to_generate}.csv"
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate synthetic data based on a schema."
+    )
+    parser.add_argument(
+        "--numrows", type=int, required=True, help="Number of rows to generate."
+    )
+    parser.add_argument(
+        "--format",
+        type=str,
+        choices=["csv", "json", "xlsx"],
+        default="csv",
+        help="Output format for the generated data (default: csv).",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="data/generated/",
+        help="Output directory for the generated file (default: data/generated/).",
     )
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    args = parser.parse_args()
 
-    # Save the generated data to the file
-    save_to_csv(synthetic_df, output_path)
+    synthetic_df = generate_synthetic_data(args.numrows)
+
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"synthetic_data_{current_datetime}_{args.numrows}"
+
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    output_path = os.path.join(args.output, filename)
+    if args.format == "csv":
+        save_to_csv(synthetic_df, f"{output_path}.csv")
+    elif args.format == "json":
+        save_to_json(synthetic_df, f"{output_path}.json")
+    elif args.format == "xlsx":
+        save_to_excel(synthetic_df, f"{output_path}.xlsx")
+
+    print(f"Data saved to {output_path}.{args.format}")
+
+
+if __name__ == "__main__":
+    main()
