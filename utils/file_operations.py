e22b523cf7b32034cebcfe1e06f7a406c790cfa5
Noey Ignacio <ignacio.francis@aelluminate.com>
2024-12-28 11:40:06 +0800

    feat: add new file formats

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -1,9 +1,23 @@
-def save_to_csv(df, filename="synthetic_data.csv"):
-    """Saves the DataFrame to a CSV file."""
+def save_to_csv(df, filename):
     try:
-        df.to_csv(
-            filename, index=False, encoding="utf-8"
-        )  # Important for special characters
-        print(f"Data saved to {filename}")
+        df.to_csv(filename, index=False, encoding="utf-8")
     except Exception as e:
         print(f"Error saving to CSV: {e}")
+
+
+def save_to_json(df, filename):
+    try:
+        date_columns = df.select_dtypes(include=["datetime64"]).columns
+        for col in date_columns:
+            df[col] = df[col].dt.strftime("%Y-%m-%d")
+
+        df.to_json(filename, orient="records", lines=True, date_format="iso")
+    except Exception as e:
+        print(f"Error saving to JSON: {e}")
+
+
+def save_to_excel(df, filename):
+    try:
+        df.to_excel(filename, index=False)
+    except Exception as e:
+        print(f"Error saving to Excel: {e}")
